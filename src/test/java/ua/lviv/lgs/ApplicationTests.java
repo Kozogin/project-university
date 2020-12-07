0ec58d6d81bc6b897d6027e9ab0a1dcf99b8685a
LogosITAcademy <Tester99037@gmail.com>
2020-12-07 19:59:22 +0200

    2 test

--- src/test/java/ua/lviv/lgs/ApplicationTests.java
+++ src/test/java/ua/lviv/lgs/ApplicationTests.java
@@ -16,11 +16,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import ua.lviv.lgs.dao.ApplicantRepository;
+import ua.lviv.lgs.dao.FacultyRepository;
+import ua.lviv.lgs.dao.NameOfLessonRepository;
 import ua.lviv.lgs.dao.UserRepository;
 import ua.lviv.lgs.domain.Applicant;
+import ua.lviv.lgs.domain.Faculty;
+import ua.lviv.lgs.domain.NameOfLesson;
 import ua.lviv.lgs.domain.Role;
 import ua.lviv.lgs.domain.User;
 import ua.lviv.lgs.service.ApplicantService;
+import ua.lviv.lgs.service.FacultyService;
+import ua.lviv.lgs.service.NameOfLessonService;
 import ua.lviv.lgs.service.UserService;
 
 
@@ -39,8 +46,25 @@ public class ApplicationTests {
 	@Autowired
 	private ApplicantService applicantService;
 	
+	@Autowired
+	private ApplicantRepository applicantRepository;
 	
-	/*  UsertService                                                */
+	@Autowired
+	private FacultyService facultyService;
+	
+	@Autowired
+	private FacultyRepository facultyRepository;
+	
+	@Autowired
+	private NameOfLessonService nameOfLessonService;
+	
+	@Autowired
+	private NameOfLessonRepository nameOfLessonRepository;
+	
+	
+	
+	
+	/*  UsertService   ---------------------------------      */
 	
 	@Test	
 	public void testSaveUser()  {		
@@ -89,25 +113,33 @@ public class ApplicationTests {
 		user3.setPasswordConfirm("100");
 		
 		userService.save(user3);
-	}
-	
-	@Test	
-	public void testFindByAssignedId()  {	
+		
 		assertTrue(userService.findByAssignedId("id1").get().getEmail().equals("1@gmail.com"));	
 		assertFalse(userService.findByAssignedId("id1").get().getEmail().equals("91@gmail.com"));
-	}
 	
-	@Test	
-	public void findAllApplicant()  {	
-		List<User> users = userService.findAllApplicant();
-		List<User> adminRole = users.stream().filter(user -> user.getRole().equals(Role.ROLE_ADMIN)).collect(Collectors.toList());
-		List<User> userRole = users.stream().filter(user -> user.getRole().equals(Role.ROLE_USER)).collect(Collectors.toList());
-		List<User> admin = userService.getAllUsers().stream().filter(user -> user.getRole().equals(Role.ROLE_ADMIN)).collect(Collectors.toList());
+		List<User> userss = userService.findAllApplicant();
+		List<User> adminRole = userss.stream().filter(usert -> usert.getRole().equals(Role.ROLE_ADMIN)).collect(Collectors.toList());
+		List<User> userRole = userss.stream().filter(usert -> usert.getRole().equals(Role.ROLE_USER)).collect(Collectors.toList());
+		List<User> admin = userService.getAllUsers().stream().filter(usert -> usert.getRole().equals(Role.ROLE_ADMIN)).collect(Collectors.toList());
 		
 		assertTrue(adminRole.size() == 0);
 		assertTrue(userRole.size() != 0);
 		assertTrue(admin.size() == 1);
+		
+		
+		User user5 = new User();
+		user5.setAssignedId("id2000");
+		user5.setEmail("2000@gmail.com");
+		user5.setFirstName("2000");
+		user5.setLastName("2000");
+		user5.setPassword("2000");
+		user5.setPasswordConfirm("2000");
+		user5.setRole(Role.ROLE_USER);
+		
+		assertTrue(applicantService.isExist(user5));			
 	}
+	
+
 	
 	@Test	
 	public void testGetLastRegistrationUser()  {	
@@ -115,26 +147,97 @@ public class ApplicationTests {
 		assertFalse(userService.getLastRegistrationUser().getFirstName().equals("100"));
 	}
 
-	/*  ApplicantService                                                */
+	/*  ApplicantService   ---------------------------------      */
 	
 	@Test	
-	public void testSave()  {
+	public void testSaveApplicant()  {
 		List<Applicant> applicants = new ArrayList<>();
 		assertThat(applicants, hasSize(0));
 		
 		Applicant applicant = new Applicant();
 		applicant.setBallgpa(10.58);
 		applicant.setChecked(true);
-		applicantService.save(applicant);
+		applicantService.save(applicant);		
+		
+		Applicant applicant2 = new Applicant();
+		applicant2.setBallgpa(8.005);
+		applicant2.setChecked(false);		
+		applicantRepository.save(applicant2);
 		
 		applicants = applicantService.getAllApplicant();
-		assertThat(applicants, hasSize(1));
+		assertThat(applicants, hasSize(2));
 		
-//		Applicant applicantFromDb = 
-//		User userFromDb = users.get(0);
-//		assertTrue(userFromDb.getAssignedId().equals(user.getAssignedId()));
+		Applicant applicantFromDb = applicantService.getAllApplicant().get(1);
+		assertTrue(applicantFromDb.getBallgpa().equals(applicant2.getBallgpa()));
+		assertTrue(applicantFromDb.getChecked().equals(applicant2.getChecked()));
+		
+		assertTrue(applicantService.findApplicant(
+				applicantFromDb.getApplicantId()
+				).getBallgpa() == 8.005);
 		
 	}
+	
+	/*  FacultyService   ---------------------------------      */
+	
+	@Test	
+	public void testSaveFaculty()  {
+				
+		List<Faculty> facylties = new ArrayList<>();
+		assertThat(facylties, hasSize(0));
+		
+		Faculty faculty = new Faculty();
+		faculty.setName("f");
+		
+		facultyService.save(faculty);		
+		
+		Faculty faculty2 = new Faculty();
+		faculty2.setName("f2");
+		facultyRepository.save(faculty2);
+		
+		facylties = facultyService.getAllFaculty();
+		assertThat(facylties, hasSize(2));
+		
+		Faculty facultyFromDb = facylties.get(1);
+		assertTrue(facultyFromDb.getName().equals(faculty2.getName()));
+		
+		Faculty faculty3 = facultyService.getAllFaculty().get(0);
+		assertTrue(facultyService.findByFacultyId(
+				faculty3.getFacultyId()).getName() == "f");
+	}
+	
+
+		/*  NameOfLessonService   ---------------------------------      */
+	
+	@Test	
+	public void testSaveNameOfLesson()  {
+				
+		List<NameOfLesson> nameOfLessons = new ArrayList<>();
+		assertThat(nameOfLessons, hasSize(0));
+		
+		NameOfLesson nameOfLesson = new NameOfLesson();
+		nameOfLesson.setName("lesson");
+		
+		nameOfLessonService.save(nameOfLesson);		
+		
+		NameOfLesson nameOfLesson2 = new NameOfLesson();
+		nameOfLesson2.setName("lesson2");
+		nameOfLessonRepository.save(nameOfLesson2);
+		
+		nameOfLessons = nameOfLessonService.getAllLesson();
+		assertThat(nameOfLessons, hasSize(2));
+		
+		NameOfLesson facultyFromDb = nameOfLessons.get(1);
+		assertTrue(facultyFromDb.getName().equals(nameOfLesson2.getName()));
+		
+		NameOfLesson nameOfLesson3 =  nameOfLessonService.getAllLesson().get(0);
+		assertTrue(nameOfLessonService.findByLessonId(
+				nameOfLesson3.getLessonId()
+				).getName() == "lesson");
+		
+		
+	}
+	
+	
 
 
 
